0efcd959e859dfe0cef6a49c83382612e7af7ab9
Weydson Lima <weydson@raisenllc.com>
2013-09-25 02:51:21 -0700

    some code cleanup

--- src/bsearch.js
+++ src/bsearch.js
@@ -1,11 +1,6 @@
 // Did we find the composer div yet?
 var compdiv_found=0;
 
-function insert_quote_in_textarea(textarea, quote) {
-	textarea.value = textarea.value + quote;
-}
-
-
 function find_div_with(attribute, value)
 {
   var allDivs = document.getElementsByTagName('div');
@@ -21,23 +16,8 @@ function find_div_with(attribute, value)
 }
 
 function find_composer_div() {
-	// Old gmail interface
-	// var iframe = document.getElementById('canvas_frame');
-	// var idoc = iframe.contentDocument;
-	// console.log(idoc.getElementsByTagName('textarea'));
-	// tareas = idoc.getElementsByTagName('textarea');
-	// for (var i=0; i < tareas.length; i++) {
-	// 	textarea = tareas[i];
-	// 	if (textarea.getAttribute && (textarea.getAttribute('id') == ':n4' || textarea.getAttribute('id') == ':1h9' || textarea.name == 'body' )) {
-	// 		return textarea;
-	// 	}
-	// }
-	// return null;
-
-
 	// New gmail interface
-	var div = find_div_with('aria-label', 'Message Body');
-	return div;
+	return find_div_with('aria-label', 'Message Body');
 }
 
 function insert_quote_in_composer_div(composer_div) {
